9cef23581ef50aba8e3cae2536116385a4a62684
Nasreen245345 <fatimanasreen950@gmail.com>
2025-09-11 12:17:54 +0500

    modefied middleware

--- backend/routes/favourites.js
+++ backend/routes/favourites.js
@@ -3,7 +3,6 @@ const router=express.Router()
 const authMidddleware=require("../middleware/auth")
 const Favourites = require("../models/Favourites")
 const Public=require("../models/Publicrecipe")
-const upload=require("../middleware/uploadMiddleware")
 router.use(authMidddleware)
 router.get('/', async (req, res) => {
   try {
